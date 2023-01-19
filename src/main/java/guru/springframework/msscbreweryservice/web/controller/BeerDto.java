9c0a3017f2074d90716d378b981f32ea7397e7d3
Prasanth <92131126+PrasanthDsp@users.noreply.github.com>
2023-01-19 21:33:46 +0530

    initial project add. Closes #1

---
+++ src/main/java/guru/springframework/msscbreweryservice/web/controller/BeerDto.java
@@ -0,0 +1,27 @@
+package guru.springframework.msscbreweryservice.web.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BeerDto {
+
+    private UUID id;
+
+    private Integer version;
+    private OffsetDateTime createDate;
+    private OffsetDateTime lastModifiedDate;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+
+    private long upc;
+    private Integer quantity;
+}
